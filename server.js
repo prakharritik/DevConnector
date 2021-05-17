eaceeed1208442b03b9f53dfa289a165763fffdb
prakharritik <60786008+prakharritik@users.noreply.github.com>
2021-05-17 20:21:00 +0530

    Setting Up Routes

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express();
 connectDB();
 
 app.get('/',(req,res)=> res.send('API RUNNING'))
+
+app.use('/api/users' , require('./routes/api/users'));
+app.use('/api/posts' , require('./routes/api/posts'));
+app.use('/api/profile' , require('./routes/api/profile'));
+app.use('/api/auth' , require('./routes/api/auth'));
  
 const PORT = process.env.PORT || 5000;
 
